e8d4e21d768dfb7f5a50ed238a52d203063331f4
Egor Taranov <dev@taranovegor.com>
2015-10-17 13:53:00 +0300

    v0.1.0

--- app.ino
+++ app.ino
@@ -5,22 +5,86 @@
 
 Adafruit_PCD8544 display = Adafruit_PCD8544(3, 4, 5, 6, 7);
 
+int seconds;
+int minutes;
+int hours;
+int stuck;
+int menu_level;
 
-void seconds(){
-  display.clearDisplay();
-  display.print(millis()/1000);
-  display.display();
+void secadd() {
+	seconds++;
+}
+
+void stuck_off(){
+	stuck=1;
 }
 
 void setup(){
-  MsTimer2::set(1000, seconds);
-  MsTimer2::start();
-  Serial.begin(9600);
-  display.begin();
-  display.clearDisplay();
-  display.setContrast(60);
-  display.display();
+	seconds=0;
+	stuck=0;
+	menu_level=0;
+	MsTimer2::set(993, secadd); // 500ms period
+	MsTimer2::set(500, stuck_off);
+	MsTimer2::start();
+
+	Serial.begin(9600);
+	display.begin();
+	display.clearDisplay();
+	display.setContrast(60);
+	display.display();
+	pinMode(10, OUTPUT);
+	pinMode(11, OUTPUT);
+	digitalWrite(10, HIGH);
+	digitalWrite(11, HIGH);
+	if(digitalRead(10)==LOW){
+		display.print("hi!");
+		display.display();
+	}
 }
 
 void loop(){
+	display.clearDisplay();
+
+	if(seconds>59){
+		seconds=0;
+		minutes++;
+	}
+
+	if(seconds>59){
+		minutes++;
+		seconds=0;
+	}
+
+	if(minutes>59){
+		minutes=0;
+		hours++;
+	}
+
+	if(hours>23){
+		hours=0;
+	}
+
+	if(digitalRead(10)==LOW&&stuck==1){
+		stuck=0;
+		menu_level=1;
+	}
+
+	if(digitalRead(11)==LOW&&stuck==1){
+		stuck=0;
+		menu_level=0;
+	}
+
+	if(menu_level==0){
+		display.print(hours);
+		display.print(":");
+		display.print(minutes);
+		display.print(":");
+		display.print(seconds);
+		display.display();
+	}
+	if(menu_level==1){
+		display.print("test");
+		display.print(stuck);
+		display.display();
+	}
 }
