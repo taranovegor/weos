9246b814f3e8881dac35acefa6c33fbb49b5890e
Egor Taranov <dev@taranovegor.com>
2015-10-19 19:11:00 +0300

    v0.2.0

--- app.ino
+++ app.ino
@@ -18,41 +18,68 @@ void secadd() {
 	seconds++;
 }
 
-/*void header() {
-	display.drawRect(0, 0, LCDWIDTH, 11, BLACK);
-	display.fillRect(69, 3, 12, 5, BLACK);
-	display.fillRect(70, 4, 3, 3, WHITE);
-	display.drawFastVLine(68, 4, 3, BLACK);
-	display.setCursor(2,2);
-	display.print("Устройство");
-	//display.drawFastVLine(0,12,11,BLACK);
-	//12
-	display.setCursor(2,14);
-	display.print("ПО:weOS 0.1");
-	//+11=23
-	display.setCursor(2,25);
-	display.print("Дисплей:N5110");
-	//+11=34
-	display.setCursor(2,36);
-	display.print("Обн:17 Окт");
-
-}*/
-
 char* menu_points[] = {"Часы","Экран","Устройство"};
+
+int menu_prev[] = {};
 
 void header(){
 	display.drawFastHLine(0,9,LCDWIDTH,BLACK);
-	display.fillRect(69, 3, 12, 5, BLACK);
-	display.fillRect(70, 4, 3, 3, WHITE);
-	display.drawFastVLine(68, 4, 3, BLACK);
+	display.fillRect(70, 3, 12, 5, BLACK);
+	display.fillRect(71, 4, 3, 3, WHITE);
+	display.drawFastVLine(69, 4, 3, BLACK);
 	display.setCursor(2,1);
 	display.print(menu_points[2]);
+
+	display.setCursor(2,12);
+	display.print("ПО тест");
+	display.setCursor(2,21);
+	display.print("Дисплей N5110");
+	display.setCursor(2,30);
+	display.print("Обновлено");
+	display.setCursor(2,39);
+	display.print("17 Окт 21:52");
 }
 
+void menu_button(){
+	if(digitalRead(11)==LOW&&stuck==1){
+		stuck=0;
+		if(menu_level==0){
+			menu_level=10;
+		}
+		if(menu_level==10){
 
+		}
+	}
+}
+
+void menu_print(int mlvl, int ipos, int iline){
+	switch(mlvl){
+		case 0:
+			display.print(hours);
+			display.print(":");
+			display.print(minutes);
+			display.print(":");
+			display.print(seconds);
+			break;
+		case 10:
+			switch(iline){
+				case 0:
+					display.print(menu_points[0]);
+					display.print(menu_points[1]);
+					display.print(menu_points[2]);
+					break;
+				case 1:
+
+					break;
+			}
+			break;
+	}
+}
 
 void setup(){
-
+	hours=22;
+	minutes=35;
+	seconds=10;
 	//Прерывания по таймеру, добавление секунды
 	MsTimer2::set(993, secadd);
 	MsTimer2::start();
@@ -64,16 +91,20 @@ void setup(){
 	display.display();
 
 	//Инциализируем порты
+	pinMode(pin_A, INPUT);
+	pinMode(pin_B, INPUT);
 	pinMode(10, OUTPUT);
 	pinMode(11, OUTPUT);
 	digitalWrite(10, HIGH);
 	digitalWrite(11, HIGH);
 
+	currentTime = millis();
+	loopTime = currentTime;
+
 }
 
 void loop(){
-	int dEnc;//digital Encoder
-	int posEnc = 0;//position Encoder
+	currentTime = millis();
 	dEnc = digitalEncoder.read();
 	display.clearDisplay();
 
@@ -96,45 +127,15 @@ void loop(){
 		hours=0;
 	}
 
-
-
 	if(digitalRead(10)==HIGH&&stuck==0||digitalRead(11)==HIGH&&stuck==0){
 		stuck=1;
 	}
-	if(digitalRead(10)==LOW&&stuck==1){
-		stuck=0;
-		menu_level=1;
-	}
+
 
 	if(digitalRead(11)==LOW&&stuck==1){
 		stuck=0;
 		menu_level=0;
 	}
 
-	if(dEnc != posEnc){
-		header();
-
-		posEnc = dEnc;
-	}
-
-
-	header();
 	display.display();
-/*
-	if(menu_level==0){
-		display.print(hours);
-		display.print(":");
-		display.print(minutes);
-		display.print(":");
-		display.print(seconds);
-		display.display();
-	}
-	if(menu_level==1){
-		header();
-		//display.drawRect(0, 12, LCDWIDTH, 11, BLACK);
-		display.setCursor(2,14);
-		display.print(menu_points[2]);
-		display.display();
-	}
-	*/
 }
