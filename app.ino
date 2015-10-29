5eb07ec70d0a7ffa5d5b6dabebdfb028264a1f17
Egor Taranov <dev@taranovegor.com>
2015-10-29 18:05:00 +0300

    v0.3.3

--- app.ino
+++ app.ino
@@ -4,7 +4,7 @@
 #include <MsTimer2.h>
 
 Adafruit_PCD8544 display = Adafruit_PCD8544(3, 4, 5, 6, 7);
-//21/34
+
 int seconds;
 int minutes;
 int hours;
@@ -23,14 +23,120 @@ const int down = 9;//blue
 
 int dev_mode = 0;
 
+//Переменные для меню
+//Меню заголовки
+char* MenuName[16];
+//Тип меню, редакт или нет
+int MenuType[16];
+//Родитель меню
+int MenuParent[16];
+//Дети меню, в зависисмости от MenuCurPos - зависит и чей он родитель
+//Пример MenuChilds[] = {1,3,15,4};
+//макс значение пока что 3 (5110 lcd)
+int MenuChildMin[16];
+int MenuChildMax[16];
+
+//
+int curMenu;
+//Текущая позиция в меню (Menu Current Position)
+int MenuCurPos = 0;
+//Статус меню, актив изменение - нет
+int MenuStatus = 0;
+//Максимальная позиция курсора
+int maxPos = 3;
+
+void MenuSetup(){
+
+	MenuName[1]="Menu";
+	MenuType[1]=0;
+	MenuParent[1]=0;
+	MenuChildMin[1]=2;
+	MenuChildMax[1]=4;
+
+	MenuName[2]="Часы";
+	MenuType[2]=0;
+	MenuParent[2]=1;
+	MenuChildMin[2]=5;
+	MenuChildMax[2]=7;
+
+	MenuName[3]="Экран";
+	MenuType[3]=0;
+	MenuParent[3]=1;
+	MenuChildMin[3]=8;
+	MenuChildMax[3]=10;
+
+	MenuName[4]="Прочее";
+	MenuType[4]=0;
+	MenuParent[4]=1;
+	MenuChildMin[4]=0;
+	MenuChildMax[4]=0;
+
+	MenuName[5]="Будильник";
+	MenuType[5]=0;
+	MenuParent[5]=2;
+	MenuChildMin[5]=0;
+	MenuChildMax[5]=0;
+
+	MenuName[6]="Часы";
+	MenuType[6]=0;
+	MenuParent[6]=2;
+	MenuChildMin[6]=0;
+	MenuChildMax[6]=0;
+
+	MenuName[7]="Циферблат";
+	MenuType[7]=0;
+	MenuParent[7]=2;
+	MenuChildMin[7]=0;
+	MenuChildMax[7]=0;
+
+	MenuName[8]="Яркость";
+	MenuType[8]=0;
+	MenuParent[8]=0;
+	MenuChildMin[8]=0;
+	MenuChildMax[8]=0;
+
+	MenuName[9]="Подсветка";
+	MenuType[9]=0;
+	MenuParent[9]=0;
+	MenuChildMin[9]=0;
+	MenuChildMax[9]=0;
+
+	MenuName[10]="Контраст";
+	MenuType[10]=0;
+	MenuParent[10]=0;
+	MenuChildMin[10]=0;
+	MenuChildMax[10]=0;
+
+	MenuName[11]="Устройство";
+	MenuType[11]=0;
+	MenuParent[11]=0;
+	MenuChildMin[11]=0;
+	MenuChildMax[11]=0;
+
+	MenuName[12]="Перезагрузка";
+	MenuType[12]=0;
+	MenuParent[12]=0;
+	MenuChildMin[12]=0;
+	MenuChildMax[12]=0;
+
+}
+
+void printMenu(){
+	display.setCursor(2,1);
+	display.print(MenuName[curMenu]);
+	display.drawFastVLine(0, (8*MenuCurPos), 9, BLACK);
+	for(int i = MenuChildMin[curMenu]; i<MenuChildMax[curMenu]+1; i++){
+		int b;
+		b++;
+		display.setCursor(2,8*b);
+		display.print(MenuName[i]);
+	}
+}
+
+
 void secadd() {
 	seconds++;
 }
-
-const char* menu_points[] = {"Часы","Экран","Устройство"};
-int menu_level = 0;
-int menu_point = 0;
-int ml_array[16];
 
 int date(int arg){
 	//Если секунд больше 59, аннулируем переменную seconds и добавим минуту
@@ -69,50 +175,28 @@ int date(int arg){
 }
 
 void bAction(){
-	if(digitalRead(up)==LOW&&currentTime >= (loopTime + 5)){
-		loopTime = currentTime;
-		if(menu_point<=0||menu_level==0){
-			return;
-		}
-
-		menu_point--;
+	if(digitalRead(forward)==LOW&&currentTime>=(loopTime+5)){
+		loopTime=currentTime;
+		curMenu++;
 	}
-	if(digitalRead(down)==LOW&&currentTime >= (loopTime + 5)){
-		loopTime = currentTime;
-		if(menu_level==0){
-			return;
-		}
-
-		menu_point++;
+	if(digitalRead(down)==LOW&&currentTime>=(loopTime+5)){
+		loopTime=currentTime;
+		MenuCurPos++;
 	}
-	if(digitalRead(forward)==LOW&&currentTime >= (loopTime + 5)){
-		loopTime = currentTime;
-
-		if(menu_level>0){
-			ml_array[menu_level] = menu_point;
-		}
-		menu_level++;
-		menu_point=0;
+	if(digitalRead(up)==LOW&&currentTime>=(loopTime+5)){
+		loopTime=currentTime;
+		MenuCurPos--;
 	}
-	if(digitalRead(back)==LOW&&currentTime >= (loopTime + 5)){
-		loopTime = currentTime;
-		if(menu_level<=0){
-			return;
-		}
-
-		ml_array[menu_level] = 0;
-		menu_level--;
-		menu_point = ml_array[menu_level];
-	}
-}
-
-char* printMenu(){
 }
 
 void setup(){
 	//Прерывания по таймеру, добавление секунды
 	MsTimer2::set(993, secadd);
 	MsTimer2::start();
+
+	//serial!
+	Serial.begin(9600);
+
 
 	//Инциализируем дисплей
 	display.begin();
@@ -138,51 +222,21 @@ void setup(){
 	currentTime = millis()/100;
 	loopTime = currentTime;
 
+	curMenu=1;
+
+	MenuSetup();
 }
 
 void loop(){
 	currentTime = millis()/100;
 	bAction();
 	display.clearDisplay();
-	display.print("mp");
-	display.print(menu_point);
-	display.print("mv");
-	display.print(menu_level);
-	display.setCursor(0,8);
-	display.print(ml_array[0]);
-	display.print("-");
-	display.print(ml_array[1]);
-	display.print("-");
-	display.print(ml_array[2]);
-	display.print("-");
-	display.print(ml_array[3]);
-	display.print("-");
-	display.print(ml_array[4]);
-	display.print("-");
-	display.print(ml_array[5]);
-	display.print("-");
-	display.print(ml_array[6]);
-	display.print("-");
-	//display.print(ml_array[7]);
-	//display.print("-");
-	//display.print(ml_array[8]);
-	//display.print("-");
-	//display.print(ml_array[9]);
-	//display.print("-");
-	//display.print(ml_array[10]);
-	display.print("0");
-	display.print("-");
-	display.print("1");
-	display.print("-");
-	display.print("2");
-	display.print("-");
-	display.print("3");
-	display.print("-");
-	display.print("4");
-	display.print("-");
-	display.print("5");
-	display.print("-");
-	display.print("6");
-	display.print("-");
+	display.print(date(3));
+	display.print(":");
+	display.print(date(4));
+	display.print(":");
+	display.print(date(5));
+	Serial.println(Serial.read(), DEC);
+	//printMenu();
 	display.display();
 }
