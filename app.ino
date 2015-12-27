f23c036c5e8c47d780d14a0543a6986aee3e4f9b
Egor Taranov <dev@taranovegor.com>
2015-12-27 17:29:00 +0300

    v0.3.0

--- app.ino
+++ app.ino
@@ -1,5 +1,5 @@
 /*
-weOS ROM 0.2.5
+weOS ROM 0.3.0
 Board: Arduino UNO
 LCD: ILI9163C 1.44" 128x128
 время до очищения дисплея добавить!!!
@@ -63,24 +63,25 @@ __RST - 8   __LED 9
 TFT_ILI9163C lcd = TFT_ILI9163C(__CS, __DC, __RST);
 
 /*Переменные*/
-/*Для работы с таймерами*/
-long currentTime;
-long loopTime;
+/*Для работы с таймерами (?long?)*/
+unsigned int currentTime;
+unsigned int loopTime;
 /*Для работы с временем, датой*/
-unsigned int seconds;
-unsigned int minutes;
-unsigned int hours;
+byte seconds;
+int minutes;
+int hours;
 unsigned int day;
 unsigned int numWeekDay;
 unsigned int month;
 unsigned int year;
 /*Для работы с циферблатом, фикс даты*/
-unsigned int minuteFixed = 0;
-unsigned int hourFixed = 0;
-unsigned int dayFixed = 0;
+byte minuteFixed = 0;
+byte hourFixed = 0;
+byte dayFixed = 0;
 /*Для отображения дат, месяца в буквах (хех)*/
 const char* namesDays[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 const char* namesMonths[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+const int daysinMonths[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 /*Для работы с будильником*/
 //unsigned int vibrationCycle;
 const unsigned int vibrationMode[] = {100, 125, 150, 175, 200, 225, 250};
@@ -115,14 +116,16 @@ const int ACRepetition = 17;
 byte brightness;
 byte backlightTimer;
 //...
+/*Для работы с настройками*/
+int settingStep;
+int currentPer;
+int fixedSetNum;//Фиксированная цифра настроек (для удаление прошлой)
 
 /*Прочее*/
 boolean devMode = false;//Деволперский режим
 boolean printDates = false;//Спец.переменная для работы с датами
 boolean sleepMode = false;//Режим сна
 boolean renderingStatics = false;//Отрисовка статических изображений
-int currentPer;
-int fixedSetNum;//Фиксированная цифра настроек (для удаление прошлой)
 
 
 /*Переменные для работы меню/c меню*/
@@ -170,8 +173,8 @@ void MenuSetup(){
 	MenuName[3]="Screen";
 	MenuType[3]=1;
 	MenuParent[3]=1;
-	MenuChildFirst[3]=9;
-	MenuChildLast[3]=10;
+	MenuChildFirst[3]=10;
+	MenuChildLast[3]=11;
 
 	MenuName[4]="Bluetooth";
 	MenuType[4]=1;
@@ -182,8 +185,8 @@ void MenuSetup(){
 	MenuName[5]="Device";
 	MenuType[5]=1;
 	MenuParent[5]=1;
-	MenuChildFirst[5]=11;
-	MenuChildLast[5]=12;
+	MenuChildFirst[5]=12;
+	MenuChildLast[5]=13;
 
 	MenuName[6]="Alarm";
 	MenuType[6]=1;
@@ -197,35 +200,41 @@ void MenuSetup(){
 	MenuChildFirst[7]=0;
 	MenuChildLast[7]=0;
 
-	MenuName[8]="//Reserv";
-	MenuType[8]=1;
+	MenuName[8]="Date";
+	MenuType[8]=6;
 	MenuParent[8]=2;
 	MenuChildFirst[8]=0;
 	MenuChildLast[8]=0;
 
-	MenuName[9]="Brightness";
-	MenuType[9]=4;
-	MenuParent[9]=3;
+	MenuName[9]="//Reserv";
+	MenuType[9]=1;
+	MenuParent[9]=2;
 	MenuChildFirst[9]=0;
 	MenuChildLast[9]=0;
 
-	MenuName[10]="Backlight";
-	MenuType[10]=5;
+	MenuName[10]="Brightness";
+	MenuType[10]=4;
 	MenuParent[10]=3;
-	//MenuChildFirst[10]=;
-	//MenuChildLast[10]=;
+	MenuChildFirst[10]=0;
+	MenuChildLast[10]=0;
 
-	MenuName[11]="Reboot";
-	MenuType[11]=2;
-	MenuParent[11]=5;
-	MenuChildFirst[11]=0;
-	MenuChildLast[11]=0;
+	MenuName[11]="Backlight";
+	MenuType[11]=5;
+	MenuParent[11]=3;
+	//MenuChildFirs1110]=;
+	//MenuChildLas1110]=;
 
-	MenuName[12]="Information";
+	MenuName[12]="Reboot";
 	MenuType[12]=2;
 	MenuParent[12]=5;
 	MenuChildFirst[12]=0;
 	MenuChildLast[12]=0;
+
+	MenuName[13]="Information";
+	MenuType[13]=2;
+	MenuParent[13]=5;
+	MenuChildFirst[13]=0;
+	MenuChildLast[13]=0;
 }
 
 void DrawMenu(){
@@ -239,27 +248,17 @@ void DrawMenu(){
 	lcd.drawFastVLine(0,MenuCurPos*16+3,14,WHITE);
 }
 
-void printCenter(char* string, unsigned int marginTop = 3, unsigned int textSize = 2, int textColor = 0xFFFF, int debugPix = 0){
-	/*
-	Условия:
-	строка, отступ сверху, размер текста, принт процента, цвет, отладочный пиксель*
-	*= (там t не равен 7 пикселям, вместо этого там их 4). !!! (-2 обязательно)
-	*/
-	lcd.setTextColor(textColor);
-	lcd.setTextSize(textSize);
-	lcd.setCursor(64-(3*textSize)*strlen(string)+debugPix, marginTop);
-	lcd.print(string);
-}
-
 void BandSettings(){
 	currentPer=brightness*100/240;
 	if(renderingStatics==false){
-		printCenter("Brightness",3,2,WHITE,4);
+		lcd.setTextSize(2);
+		lcd.setCursor(8,3);
+		lcd.print("Brightness");
 		lcd.fillRect(4, 69, 1.2*currentPer, 10, WHITE);
 		renderingStatics=true;
 	}
 	if(currentPer!=fixedSetNum){
-		lcd.setTextColor(RED);
+		lcd.setTextColor(BLACK);
 		if(fixedSetNum==100){
 			lcd.setCursor(46,45);
 		}
@@ -277,8 +276,9 @@ void BandSettings(){
 			lcd.fillRect(1.2*currentPer-2, 69, 6, 10, WHITE);
 		}
 		fixedSetNum=currentPer;
+
 	}
-	lcd.setTextColor(CYAN);
+	lcd.setTextColor(WHITE);
 	if(currentPer==100){
 		lcd.setCursor(46,45);
 	}
@@ -291,12 +291,65 @@ void BandSettings(){
 	lcd.print(currentPer);
 }
 
-void DateSettings(){
+void TimeSettings(){
+	lcd.setTextSize(2);
+	lcd.setTextColor(WHITE);
+	lcd.setCursor(42,3);
+	lcd.print("Time");
+	//main
 	lcd.setTextSize(3);
 	lcd.setCursor(26,50);
+	if(time(4)<10){
+		lcd.print("0");
+	}
 	lcd.print(time(4));
 	lcd.print(":");
+	if(time(5)<10){
+		lcd.print("0");
+	}
 	lcd.print(time(5));
+	switch(settingStep){
+		case 0:
+			lcd.drawFastHLine(26, 76, 33, WHITE);
+			break;
+		case 1:
+			lcd.drawFastHLine(68, 76, 33, WHITE);
+			break;
+	}
+}
+
+void DateSettings(){
+	lcd.setTextSize(2);
+	lcd.setTextColor(WHITE);
+	if(renderingStatics==false){
+		lcd.setCursor(42,3);
+		lcd.print("Date");
+		renderingStatics=true;
+	}
+	//День месяца
+	lcd.setCursor(10,56);
+	lcd.print(namesDays[time(3)-1]);
+	//Дата
+	lcd.setCursor(52,56);
+	if(time(2)<10){
+		lcd.print("0");
+	}
+	lcd.print(time(2));
+	//Месяц
+	lcd.setCursor(84,56);
+	lcd.print(namesMonths[time(1)-1]);
+	switch(settingStep){
+		case 0:
+			lcd.drawFastHLine(10, 74, 34, WHITE);
+			break;
+		case 1:
+			lcd.drawFastHLine(52, 74, 22, WHITE);
+			break;
+
+		case 2:
+			lcd.drawFastHLine(84, 74, 34, WHITE);
+			break;
+	}
 }
 
 /*Установка даты и времени от компьютера*/
@@ -330,7 +383,7 @@ unsigned int time(int arg){
 		numWeekDay++;
 	}
 	//Добавляем массив с количеством дней в месяце, ноль для того что бы небыло мороки с отсётом месяцев с нуля (0-11 вместо 1-12)
-	const int daysinMonths[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+//перемещено
 	//Если день > количества дней в месяц [текущий], обнуляем days и добавляем месяц
 	//Вводит в ступор инциализация месяца, разобрать нужно
 	if(day>daysinMonths[month]||day==0){
@@ -347,9 +400,30 @@ unsigned int time(int arg){
 	}
 	//Готовим (на печи) строку для возращения
 	//year = 0 months = 1 days = 2 numWeekDay = 3 hours = 4 minutes = 5 seconds = 6
-	int retstr[] = {year, month, day, numWeekDay, hours, minutes, seconds};
+	if(arg==0){
+		return year;
+	}
+	else if(arg==1){
+		return month;
+	}
+	else if(arg==2){
+		return day;
+	}
+	else if(arg==3){
+		return numWeekDay;
+	}
+	else if(arg==4){
+		return hours;
+	}
+	else if(arg==5){
+		return minutes;
+	}
+	else if(arg==6){
+		return seconds;
+	}
+	//int retstr[] = {year, month, day, numWeekDay, hours, minutes, seconds};
 	//Возращаем строку
-	return retstr[arg];
+	//return retstr[arg];
 }
 
 void DigitalClockFace(){
@@ -417,7 +491,9 @@ void DigitalClockFace(){
 			lcd.print("0");
 		}
 		lcd.print(time(5));
-		printDates=true;
+		if(printDates==false){
+			printDates=true;
+		}
 	}
 }
 
@@ -457,6 +533,7 @@ void setup(){
 	//backlightTimer=EEPROM.read(backlightTimer)*60*10*10;
 	/*Инциализация экрана*/
 	lcd.begin();
+	lcd.setBitrate(2);
 	lcd.setFont(&defaultFont);
 	/*Установка типа пинов*/
 	//Аналоговые
@@ -479,12 +556,12 @@ void setup(){
 	//seconds=conv2d(__TIME__ + 6)+6;
 	//minutes=conv2d(__TIME__ + 3);
 	//hours=conv2d(__TIME__);
-	seconds=50;
-	minutes=12;
-	hours=14;
-	day=26;
-	numWeekDay=6;
-	month=12;//7[proverka]
+	seconds=0;
+	minutes=EEPROM.read(minuteAddress);
+	hours=EEPROM.read(hourAddress);
+	day=EEPROM.read(dayAddress);
+	numWeekDay=EEPROM.read(numWeekDayAddress);
+	month=EEPROM.read(monthAddress);//7[proverka]
 	year=2015;
 	/*Установка fix-дат*/
 	minuteFixed=time(5);
@@ -507,10 +584,15 @@ void loop(){
 	//Выход из меню, если небыло действий на протяжении 600мс
 	if(currentTime>=loopTime+600&&MenuLevel>0){
 		loopTime=currentTime;
-		lcd.clearScreen();
 		printDates=false;
 		MenuLevel=0;
 		MenuCurPos=0;
+		switch(MenuType[MenuLevel]){
+			case 4:
+				brightness=EEPROM.read(brightnessAddress);
+				break;
+		}
+		lcd.clearScreen();
 	}
 	//Действие при нажатии на ok/back
 	if(analogRead(ok)==0&&currentTime>=loopTime+5){
@@ -519,28 +601,60 @@ void loop(){
 		switch(MenuType[MenuLevel]){
 			case 4:
 				EEPROM.write(brightnessAddress, brightness);
+				MenuCurPos=MenuLevel-MenuChildFirst[MenuParent[MenuLevel]];
 				MenuLevel=MenuParent[MenuLevel];
-				lcd.clearScreen();
+				break;
+			case 5:
+				settingStep++;
+				if(settingStep>1){
+					EEPROM.write(minuteAddress, minutes);//День недели
+					EEPROM.write(hourAddress, hours);//День месяца
+					settingStep=0;
+					MenuLevel=MenuParent[MenuLevel];
+				}
+				break;
+
+			case 6:
+				settingStep++;
+				if(settingStep>2){
+					EEPROM.write(numWeekDayAddress, numWeekDay);//День недели
+					EEPROM.write(dayAddress, day);//День месяца
+					EEPROM.write(monthAddress, month);//Mесяц
+					settingStep=0;
+					MenuLevel=MenuParent[MenuLevel];
+				}
 				break;
 			default:
 				MenuLevel=MenuChildFirst[MenuLevel]+MenuCurPos;
 				MenuCurPos=0;
-				lcd.clearScreen();
 				break;
 		}
+		lcd.clearScreen();
 	}
 	if(analogRead(back)==0&&MenuLevel!=0&&currentTime>=loopTime+5){
 		loopTime=currentTime;
 		renderingStatics=false;
-		MenuCurPos=0;
-		MenuLevel=MenuParent[MenuLevel];
-		if(MenuLevel==0){//Если после обновления menulevel = 0, тогда обн. принт
-			printDates=false;
+		switch(MenuType[MenuLevel]){
+			case 6:
+				settingStep--;
+				if(settingStep<0){
+					settingStep=0;
+					MenuCurPos=MenuLevel-MenuChildFirst[MenuParent[MenuLevel]];
+					MenuLevel=MenuParent[MenuLevel];
+				}
+				break;
+			default:
+				MenuCurPos=MenuLevel-MenuChildFirst[MenuParent[MenuLevel]];
+				MenuLevel=MenuParent[MenuLevel];
+				if(MenuLevel==0){//Если после обновления menulevel = 0, тогда обн. принт
+					printDates=false;
+				}
+				break;
 		}
 		lcd.clearScreen();
 	}
 	//Действие при нажатии на up/down
-	if(analogRead(up)==0&&currentTime>=loopTime+1){
+	if(analogRead(up)==0&&currentTime>=loopTime+5){
 		loopTime=currentTime;
 		switch(MenuType[MenuLevel]){
 			case 1:
@@ -559,9 +673,75 @@ void loop(){
 				//lcd.fillRect(4, 69, 120, 10, BLACK);
 				analogWrite(backlight, brightness);
 				break;
+			case 5:
+				switch(settingStep){
+					case 0:
+						fixedSetNum=hours;
+						hours++;
+						if(hours>23){
+							hours=0;
+						}
+						lcd.setTextColor(BLACK);
+						lcd.setCursor(26,50);
+						if(fixedSetNum<10){
+							lcd.print("0");
+						}
+						lcd.print(fixedSetNum);
+						fixedSetNum=hours;
+						break;
+					case 1:
+						fixedSetNum=minutes;
+						minutes++;
+						if(minutes>59){
+							minutes=0;
+						}
+						lcd.setTextColor(BLACK);
+						lcd.setCursor(68,50);
+						if(fixedSetNum<10){
+							lcd.print("0");
+						}
+						lcd.print(fixedSetNum);
+						fixedSetNum=minutes;
+						break;
+				}
+				break;
+			case 6:
+				switch(settingStep){
+					case 0:
+						fixedSetNum=numWeekDay;
+						numWeekDay++;
+						lcd.setTextColor(BLACK);
+						lcd.setCursor(10,56);
+						lcd.print(namesDays[fixedSetNum-1]);
+						fixedSetNum=numWeekDay;
+						break;
+					case 1:
+						fixedSetNum=day;
+						day++;
+						if(day==32){
+							month--;
+						}
+						lcd.setTextColor(BLACK);
+						lcd.setCursor(52,56);
+						if(fixedSetNum<10){
+							lcd.print("0");
+						}
+						lcd.print(fixedSetNum);
+						fixedSetNum=day;
+						break;
+					case 2:
+						fixedSetNum=month;
+						month++;
+						lcd.setTextColor(BLACK);
+						lcd.setCursor(84,56);
+						lcd.print(namesMonths[fixedSetNum-1]);
+						fixedSetNum=month;
+						break;
+				}
+				break;
 		}
 	}
-	if(analogRead(down)==0&&currentTime>=loopTime+1){
+	if(analogRead(down)==0&&currentTime>=loopTime+5){
 		loopTime=currentTime;
 		switch(MenuType[MenuLevel]){
 			case 1:
@@ -580,6 +760,78 @@ void loop(){
 				//lcd.fillRect(4, 69, 120, 10, BLACK);
 				analogWrite(backlight, brightness);
 				break;
+			case 5:
+				switch(settingStep){
+					case 0:
+						fixedSetNum=hours;
+						hours--;
+						if(hours<0){
+							hours=23;
+						}
+						lcd.setTextColor(BLACK);
+						lcd.setCursor(26,50);
+						if(fixedSetNum<10){
+							lcd.print("0");
+						}
+						lcd.print(fixedSetNum);
+						fixedSetNum=hours;
+						break;
+					case 1:
+						fixedSetNum=minutes;
+						minutes--;
+						if(hours<0){
+							hours=59;
+						}
+						lcd.setTextColor(BLACK);
+						lcd.setCursor(68,50);
+						if(fixedSetNum<10){
+							lcd.print("0");
+						}
+						lcd.print(fixedSetNum);
+						fixedSetNum=minutes;
+						break;
+				}
+				break;
+			case 6:
+				switch(settingStep){
+					case 0:
+						fixedSetNum=numWeekDay;
+						numWeekDay--;
+						if(numWeekDay<1){
+							numWeekDay=7;
+						}
+						lcd.setTextColor(BLACK);
+						lcd.setCursor(10,56);
+						lcd.print(namesDays[fixedSetNum-1]);
+						fixedSetNum=numWeekDay;
+						break;
+					case 1:
+						fixedSetNum=day;
+						day--;
+						if(day<1){
+							day=daysinMonths[month];
+						}
+						lcd.setTextColor(BLACK);
+						lcd.setCursor(52,56);
+						if(fixedSetNum<10){
+							lcd.print("0");
+						}
+						lcd.print(fixedSetNum);
+						fixedSetNum=day;
+						break;
+					case 2:
+						fixedSetNum=month;
+						month--;
+						if(month<1){
+							month=12;
+						}
+						lcd.setTextColor(BLACK);
+						lcd.setCursor(84,56);
+						lcd.print(namesMonths[fixedSetNum-1]);
+						fixedSetNum=month;
+						break;
+				}
+				break;
 		}
 	}
 	//
@@ -594,14 +846,35 @@ void loop(){
 			BandSettings();
 			break;
 		case 5:
+			seconds=0;
+			TimeSettings();
+			break;
+		case 6:
 			DateSettings();
 			break;
 	}
-	//
 	switch(MenuLevel){
-		case 11:
+		case 12:
 			analogWrite(backlight, 0);
 			resetFunc();
+			break;
+		case 13:
+			lcd.setCursor(3,0);
+			lcd.setTextSize(2);
+			lcd.print("Information");
+			lcd.setCursor(2,16);
+			lcd.print("OS version");
+			lcd.setCursor(2,32);
+			lcd.print("0.3 alpha");
+			lcd.setCursor(2,48);
+			lcd.print("SOC");
+			lcd.setCursor(2,64);
+			lcd.print("ATmega328p");
+			lcd.setCursor(2,80);
+			lcd.print("Free RAM");
+			lcd.setCursor(2,96);
+			lcd.print(freeMemory());
+			lcd.print("/2048Kb");
 			break;
 	}
 }
